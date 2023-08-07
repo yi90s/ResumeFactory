b1991e819f1a6da4ddca94cd400464d0feda6b17
Yi Peng <ypeng.ollie@gmail.com>
2023-08-07 14:08:59 -0500

    initial commit

---
+++ microservices/prompt-service/src/main/java/com/resume/prompt/controllers/Greeting.java
@@ -0,0 +1,3 @@
+package com.resume.prompt.controllers;
+
+public record Greeting(long id, String content) { }
---
+++ microservices/prompt-service/src/test/java/com/resume/prompt/PromptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume.prompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
